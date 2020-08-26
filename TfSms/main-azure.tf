3417e5d6db23e9ba6a2ca7a83473ec97921daae3
Richard Cove <39362230+rcove@users.noreply.github.com>
2020-08-26 14:40:25 +1000

    updates

--- TfSms/main-azure.tf
+++ TfSms/main-azure.tf
@@ -16,9 +16,10 @@ variable azure_tenant_id {
 }
 
 provider "azurerm" {
-  version = "=1.35.0"
-  subscription_id = "var.azure_subscription_id"
-  client_id       = "var.azure_client_id"
-  client_secret   = "var.azure_client_secret"
-  tenant_id       = "var.azure_tenant_id"
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+
+  features {}
 }
